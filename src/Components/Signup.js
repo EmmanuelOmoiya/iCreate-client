6458641211da7b9e482be9357717a9f5f2dc22b9
Dev_coder <emmanuelomoiya6@gmail.com>
2022-03-26 15:01:28 +0100

    initial progress

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -10,7 +10,7 @@ const SignUp = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [interchange, setInterChange] = useState(false);
-
+    const [isPending, setIsPending] = useState(false);
 
     const put = () =>{
         setInterChange(!interchange);
@@ -31,6 +31,8 @@ const SignUp = () => {
             return;
         }
 
+        setIsPending(true);
+
         try{
             const config = {
                 headers: {
@@ -39,6 +41,7 @@ const SignUp = () => {
             };
             const { data } = await axios.post("https://icreate-server.herokuapp.com/api/user", {fullName, email, password}, config)
                 localStorage.setItem("userInfo", JSON.stringify(data));
+                setIsPending(false);
                 window.location.replace('/dashboard');
         } catch (error){
             Show("An Error occured");
@@ -70,7 +73,7 @@ const SignUp = () => {
                     <input className='signin-input' type={interchange ? 'text' : 'password'} placeholder='Password' value={password} onChange={(e)=>setPassword(e.target.value)}/>
                     <span className="icon-change"><FaRegEyeSlash onClick={put} className='iconst noeyes'/><IoEyeSharp onClick={put}  className='iconst eyes' /></span>
                     <p className="forgot">I agree to the <Link to='#' className='link'>Terms and Condition</Link> and <Link to="#" className='link'>Privacy Policy</Link></p>
-                    <button className="submit" onClick={submitForm}>Sign Up</button>
+                    { isPending ? <button className="submit" disabled>Signing Up...</button> : <button className="submit"onClick={submitForm}>Sign Up</button>}
                     <span className="under-btn">
                         <p className="dont">Have an account already?</p>
                         <Link to="/login" className='link'><p className="go-dont">Sign In</p></Link>
